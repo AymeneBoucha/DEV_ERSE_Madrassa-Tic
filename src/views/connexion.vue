a9b654e2656a4d0ddd1e2b91211bac0d922f545c
Houssam <jh_achab@esi.dz>
2022-03-26 14:02:24 +0100

    second commit

--- src/views/connexion.vue
+++ src/views/connexion.vue
@@ -1,15 +1,37 @@
 <template>
 <v-app>
        <div class="topnav"> <h1>MADRASA-TIC</h1></div>
-        <img src="@/assets/connexion.png"/>
-        <div class="infos">
-          <v-row >
-             <v-col>
-                 <v-card-text  >
-                   <h1 class="authentification">Authentification</h1>
+        <!-- <img src="@/assets/Confirmer.png"/> -->
+        <table class="infos" width="100%">
+          <v-row>
+            <v-card-text class="text-center">
+            <h3 class="modifier">Modifier votre compte</h3>
+            </v-card-text>
+          </v-row>
+          <v-row>
+              <v-col width="40%">      
                  <v-form>
-                  <v-text-field  class="text-center mt-10"
-                    label="Adresse email"
+                  <p class="toModif">Nom d'utilisateur :</p>
+                  <p class="toModif">Email :</p>
+                  <p class="toModif">Numéro de telephone :</p>
+                  <p class="toModif">Date de naissance :</p>
+                  <p class="toModif">Mot de passe :</p>
+                  <p class="toModif">Photo de profile :</p>
+                 </v-form>
+             </v-col>
+             <v-col width="30%">
+                 <v-card-text>         
+                 <v-form>
+                  <v-text-field  class="text-center mt-0"
+                   label=""
+                   v-model="object.name"
+                   type="name"
+                   prepend-icon="badge"
+                   color="blue"
+                  ></v-text-field>
+                  <v-text-field  class="text-center mt-0"
+                    label=""
+                    suffix="@esi-sba.dz"
                     v-model="object.email"
                     required
                    :rules="[(v) => !!v || 'veuillez saisir votre adresse email']"
@@ -17,30 +39,51 @@
                    prepend-icon="email"
                    color="blue"
                   ></v-text-field>
-                  <v-text-field class="text-center mt-1"
-                   label="Mot de passe"
+                  <v-text-field  class="text-center mt-0"
+                    label=""
+                    prefix="+213"
+                   type="phone"
+                   prepend-icon="phone"
+                   color="blue"
+                  ></v-text-field>
+                  <v-text-field class="text-center mt-0"
+                   label=""
+                   type="date"
+                   prepend-icon="event"
+                  ></v-text-field>
+                  <v-text-field class="text-center mt-0"
+                   label=""
                    v-model="object.password"
                    type="password"
                    required
                    :rules="[(v) => !!v || 'veuillez saisir votre mot de passe']"
                    prepend-icon="lock"
                   ></v-text-field>
+                  <v-file-input class="text-center mt-0"
+                   label=""
+                   apend-icon="file download"
+                  ></v-file-input>
                   <p>{{ email }}{{ password }}</p>
                  </v-form>
-                 <router-link to="/mdp_oublie" class=" mdp">Mot de passe oublié ?</router-link>
-                <v-card-actions >
-                    <v-btn  class="connexion" color="#0E84D9" @click="login()">
-                      Se Connecter
-                    </v-btn>
-               </v-card-actions>
-                 <div class="text-center mt-15">
-                 <p class="compte white--text">Vous n'avez pas de compte?</p> 
-                   <h3 class="inscription"><a href="/accueil">Inscrivez-vous!</a></h3>
-                </div>
+                <!-- <router-link to="/mdp_oublie" class=" mdp">Mot de passe oublié ?</router-link> -->
                </v-card-text>
              </v-col>
           </v-row>
-        </div>
+          <v-row>
+              <v-card-actions >
+                    <v-btn  class="Confirmer" color="#0E84D9" @click="login()">
+                      Confirmer
+                    </v-btn>
+              </v-card-actions>
+          </v-row>
+        </table>
+ 
+        <div class="sidenav"></div>
+        <div class="pagePrincipale"> <h2>Page principale</h2></div>
+        <div class="alerte"> <h2>Signaler alerte</h2></div>
+        <div class="annonce"> <h2>Ecrire annonce</h2></div>
+        <div class="propos"> <h2>A propos de site</h2></div>
+        <div class="profile"> <h2>Houssam</h2></div>
   </v-app>
 </template>
 
@@ -55,7 +98,8 @@ export default {
   data(){
     return {
         object : {
-            email: "",
+            name: "example name",
+            email: "example_email",
             password: "",
         }
     };
@@ -70,7 +114,7 @@ export default {
           router.push({ name: 'acceuil' });
       } catch(e) {
           alert('Adresse ou mot de passe incorrect');
-          router.push({ name: 'connexion' });
+          router.push({ name: 'Confirmer' });
       }
     }, 
   },
@@ -82,48 +126,67 @@ export default {
   background-color: #C5CFD6;
   padding: 5px;
   position: absolute;
-  top: 100px;
-  right:3.5cm;
-  width: 400px;
-  height: 500px;
+  top: 130px;
+  right:6cm;
+  width: 700px;
+  height: 600px;
 }
-.authentification{
+.modifier{
   color: white;
   text-align: center;
   position: relative;
-  top: 10px;
+  top: 15px;
   font-size: 35px;
+}
+.toModif{
+  color: rgb(100, 100, 100);
+  text-align: left;
+  position: relative;
+  padding-top: 20px;
+  top: 10px;
+  left: 20px;
+  font-size: 20px;
 }
 .topnav {
   background-color: #C5CFD6;
   color: #0084FE;
   text-align: center;
-  width: 1400px;
+  width: 1450px;
   height: 60px;
+  padding: 10px;
   float: left;
+  z-index: 1;
 }
-.connexion{
+.Confirmer{
+  color:white;
   background-color: #0E84D9;
   text-align: center;
-  width: 60%;
-  left:2cm;
-  padding: 12px 20px;
+  width: 100%;
+  left:6.5cm;
+  padding: 0px;
   position:relative;
-  color:white;
-  top:50px;
-  border: 1px solid #238DEF;
-  border-radius: 20px;
+  top:-20px;
+  border: 30px solid #238DEF;
+  border-radius: 30px;
   font-size: 30px;
 }
-.mdp{color:#0E84D9;font-size: 16px;}
+.mdp{
+  color:#0E84D9;
+  font-size: 16px;
+  position:relative;
+  left: 1cm;
+  top:250px;
+}
 .compte{
   font-size: 17px;
   position:relative;
-  top:40px;
+  left: 2.55cm;
+  top:345px;
 }
 .inscription{
    position:relative;
-  top:30px;
+  top:355px;
+  left: 3.5cm;
   font-size: 18px;
 }
 img {
@@ -134,5 +197,46 @@ img {
   top: 2cm;
   padding: 12px 20px;
 }
-
+.sidenav {
+  background-color: #8D99AE;
+  color: white;
+  position: absolute;
+  text-align: center;
+  width: 300px;
+  height: 1000px;
+}
+.pagePrincipale{
+  color: white;
+  position: relative;
+  top: 30px;
+  left: 2.5cm;
+}
+.alerte{
+  color: white;
+  position: relative;
+  top: 60px;
+  left: 2.5cm;
+}
+.annonce{
+  color: white;
+  position: relative;
+  top: 90px;
+  left: 2.5cm;
+}
+.propos{
+  color: white;
+  position: relative;
+  top: 120px;
+  left: 2.5cm;
+}
+.profile{
+  background-color: #2B2D42;
+  text-align: right;
+  width: 300px;
+  padding: 20px 45px;
+  position:relative;
+  color:white;
+  top:510px;
+  font-size: 20px;
+}
 </style>
